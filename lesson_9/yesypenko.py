4a62338876c72a66ec5516aeb1b1b0c756540cec
yeqorkaa <egoresipenka@gmail.com>
2022-07-04 23:05:35 +0300

    Add lesson_10:home task

---
+++ lesson_9/yesypenko.py
@@ -0,0 +1,107 @@
+
+import json
+import random
+from lesson_9.home_task import is_admin, find_product
+from lesson_9.home_task import write_user_to_file
+
+
+
+def distance(x1, y1, x2, y2):
+    """
+    Function get coordinates of two points and returns a distance between them
+    :param x1:
+    :param y1:
+    :param x2:
+    :param y2:
+    :return:
+    """
+    return 0
+
+
+def quadratic_equation(a, b, c):
+    """
+    Return the roots of the quadratic equation
+    a * x^2 + b*x + c = 0
+    :param a: first argument, couldn't not be 0
+    :param b: second argument
+    :param c: third argument
+    :return: square equation roots
+    """
+    assert a != 0
+    x1, x2 = None, None
+    # insert your code here
+    return x1, x2
+
+
+def is_admin(user: dict) -> bool:
+    """
+    Gets a user and return if the user is admin or not
+    User is a dict, the info about user admin status may be present in fields:
+    'is_admin', 'admin', 'super_user', 'superuser', 'is_root', 'root'
+    :param user: dictionary with user info
+    :return: True or False
+    """
+    is_admin = False
+    # insert your code here
+    return is_admin
+
+
+def generate_email(names, domains):
+    """
+    Даны списки names и domains (создать самостоятельно).
+    Написать функцию для генерирования e-mail в формате:
+    фамилия.число_от_100_до_999@строка_букв_длинной_от_5_до_7_символов.домен
+    фамилию и домен брать случайным образом из заданных списков переданных в функцию в виде параметров.
+    Строку и число генерировать случайным образом.
+
+    Пример использования функции:
+    names = ["king", "miller", "kean"]
+    domains = ["net", "com", "ua"]
+    e_mail = create_email(domains, names)
+    print(e_mail)
+    >>>miller.249@sgdyyur.com
+    """
+    email = ''
+    return email
+
+
+def find_product(all_products, search_word):
+    """
+    Create a function to search among products and find by keywords
+    :param all_products: iterable of some products
+    :param search_word: string to search in product name or product description, case-insensitive
+    :return: list of products
+    """
+    found = []
+    # insert your code here
+    return found
+
+
+def write_user_to_file(users, search_name):
+    """
+    Write a function, that takes a user_name or last name find the user and write the result
+    to a file <FirstName_LastName>.txt
+    The user data inside should be written in a pretty format:
+
+    Key     :       Value
+    :param users: iterable of users
+    :param search_name: First or Last name of the user to search, case insensitive
+    :return:  str file_name of the user if found or empty string if not
+    """
+    # 1. for user in users:
+    #   check user firstName or user lastName
+    # 2. if user was found:
+    # create filename
+    # and create file:
+    # for k, v in user.items():
+    #  write to file
+    # return filename
+    # 3. user was not found:
+    # return ''
+    pass
+
+if __name__ == '__main__':
+    # update_users_json()
+    check_product_search()
+    check_admin()
+    check_file_write()
